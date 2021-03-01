9f8bc73dbdaf9484c864a0d0169cbf3a92530d0f
Samu Rahkila <samu.rahkila@gmail.com>
2021-03-01 23:30:32 +0200

    How Much Time - now it is actually partially complete!

--- HOW_MUCH_TIME/script.js
+++ HOW_MUCH_TIME/script.js
@@ -1,13 +1,36 @@
 function calculate() {
-    let enddate = document.querySelector("#enddate").value;
-    let eventname = document.querySelector("#eventname").value;
+    let endDate = document.querySelector("#enddate").value;
+    let eventName = document.querySelector("#eventname").value;
+    let startDate = document.querySelector("#startdate").value;
 
-    const TOTAL = Date.parse(enddate) - Date.parse(new Date());
-    const SECONDS = Math.floor((TOTAL/1000)%60);
-    const MINUTES = Math.floor((TOTAL/1000/60)%60);
-    const HOURS = Math.floor((TOTAL/(1000*60*60))%24);
-    const DAYS = Math.floor(TOTAL/(1000*60*60*24));
+    let parseEnd = Date.parse(endDate);
+    let dateNow = new Date();
 
-    answer.textContent = `${TOTAL} ${DAYS} ${HOURS} ${MINUTES} ${SECONDS} ${eventname}`;
+    let minutes = 1000*60;
+    let hours = minutes*60;
+    let days = hours*24;
 
+    // How many days overall.
+    let howMuch = Math.round((parseEnd-dateNow)/days);
+
+    yourevent.textContent = `Event: "${eventName}"`;
+    answer.textContent = `Days left until event: ${howMuch}`;
+
+
+    //How many business days.
+    let weekendCounter = 0;
+    let theDate = dateNow;
+
+    while (theDate<=parseEnd) {
+        let weekDay = theDate.getDay();
+
+        if(weekDay==6 || weekDay==0){
+            weekendCounter++;
+        }
+        theDate.setDate(theDate.getDate() + 1);
+    }
+
+    secondanswer.textContent = `Business days left until event: ${howMuch-weekendCounter}`;
+    
+    //For the start date I think I might adjust all of the code above such that it will consider the start date but default to 'new Date()' if a valid date is not set for the start date.
 }
